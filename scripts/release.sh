d7ca93e449e547bc5039c74f59b2a8091c2bc810
Trung Dang <trungdt@absoft.vn>
2019-03-05 23:07:30 +0800

    fix: update build script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,18 +2,18 @@
 set -e
 export SHOPBASH_HOME=$HOME/.shopbash
 source ${SHOPBASH_HOME}/utils/colors
-
-CURRENT_VERSION="$(node -p "require('./lerna.json').version")"
+# Bump version frist but dont push
+shopbash ms bump --no-push --no-tag
+NEXT_VERSION="$(node -p "require('./package.json').version")"
+# release packages
 npm run release
-NEXT_VERSION="$(node -p "require('./lerna.json').version")"
-if [ "$CURRENT_VERSION" != "$NEXT_VERSION" ]; then
-  cGreen "Publishing packages to Nexus"
-  npm run clean
-  npm install
-  npm run bootstrap
-  npm run publish
-  # after publish, there are some unnecessary changes in package.json files that to be clean
-  # git reset --hard
-  npm run clean:build-artifacts
-  cGreen "Packages released to Nexus"
-fi
+cGreen "Publishing packages to Nexus"
+npm run clean
+npm install
+npm run bootstrap
+npm run publish
+# clean after publish
+npm run clean:build-artifacts
+cGreen "Packages released to Nexus"
+#
+cPurple "Now you can run shopbash ms release [country]"
--- src/packages/cashbacks/index.js
+++ src/packages/cashbacks/index.js
@@ -1,5 +1,5 @@
 // change in cashbacks 
-// update 3 in cashbacks.
+// update 4 in cashbacks.
 exports = {
   file: 'cashbacks'
 }
